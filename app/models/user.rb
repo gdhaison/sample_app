b64711b3f94892966eb1be6bab74d63973624140
gdhaison <vuhaisonhb456@gmail.com>
2019-01-08 18:31:33 +0700

    Chapter_8: Sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,21 @@ class User < ApplicationRecord
     format: {with: VALID_EMAIL_REGEX}, uniqueness: true
   validates :password, presence: true,
     length: {minimum: Settings.user_pass_length_min}
-  before_save :email_downcase
+  before_save :downcase_email
   has_secure_password
-  
+
+  class << self
+    def digest string
+      cost = if ActiveModel::SecurePassword.min_cost
+               BCrypt::Engine::MIN_COST
+             else
+               BCrypt::Engine.cost
+             end
+      BCrypt::Password.create string, cost: cost
+    end
+  end
   private
-  
+
   def downcase_email
     email.downcase!
   end
