5b9ac3019344032bb3bdd392394528dced3803f5
gdhaison <vuhaisonhb456@gmail.com>
2019-01-16 00:04:03 +0700

    Chapter 10:Updating, showing, and deleting users

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem "bootsnap", ">= 1.1.0", require: false
 gem "bootstrap-sass", "3.3.6"
 gem "coffee-rails", "~> 4.2"
 gem "config", "~> 1.7", ">= 1.7.1"
+gem "faker"
 gem "jbuilder", "~> 2.5"
 gem "puma", "~> 3.11"
 gem "rails", "~> 5.2.2"
@@ -15,6 +16,8 @@ gem "sass-rails", "~> 5.0"
 gem "sqlite3"
 gem "turbolinks", "~> 5"
 gem "uglifier", ">= 1.3.0"
+gem "kaminari", "~> 1.1", ">= 1.1.1"
+gem "bootstrap-kaminari-views", "~> 0.0.5"
 
 group :development, :test do
   gem "byebug", platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: %i(edit update)
+  before_action :correct_user, only: %i(edit update)
+  before_action :admin_user, only: :destroy
+
+  def index
+    @users = User.page(params[:page]).per Settings.split
+  end
+  
   def new
     @user = User.new
   end
@@ -8,10 +16,10 @@ class UsersController < ApplicationController
 
     if @user.save
       log_in @user
-      flash[:success] = t "sample_app"
+      flash[:success] = t ".sample_app"
       redirect_to @user
     else
-      flash[:danger] = t "signup_err"
+      flash[:danger] = t ".signup_err"
       render :new
     end
   end
@@ -24,10 +32,50 @@ class UsersController < ApplicationController
     redirect_to signup_path
   end
 
+  def edit; end
+
+  def update
+    @user = User.find params[:id]
+
+    if @user.update_attributes user_params
+      flash[:success] = t "updated"
+      redirect_to @user
+    else
+      flash[:danger] = t "notice_show"
+      render "edit"
+   end
+  end
+
+  def destroy
+    if User.find(params[:id]).destroy
+      flash[:success] = t "deleted"
+      redirect_to users_url
+    else
+      flash[:success] = t "cantdelete"
+      render :users
+    end
+  end
+
   private
 
   def user_params
-    params.require :user.permit :name, :email, :password,
+    params.require(:user).permit :name, :email, :password,
       :password_confirmation
   end
-end
+
+  def correct_user
+    @user = User.find params[:id]
+    redirect_to(root_url) unless @user == current_user
+  end
+
+  def logged_in_user
+    return false if logged_in?
+      store_location
+      flash[:danger] = t ".plslogin"
+      redirect_to login_url
+    end
+  end
+
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
